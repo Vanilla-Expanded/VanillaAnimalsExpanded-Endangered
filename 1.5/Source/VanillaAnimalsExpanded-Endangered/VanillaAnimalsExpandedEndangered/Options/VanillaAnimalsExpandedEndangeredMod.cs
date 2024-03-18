a5ef04cf1de3400c1e4fa27ba337102d03cf783c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-18 14:59:54 +0100

    1.5 update

---
+++ 1.5/Source/VanillaAnimalsExpanded-Endangered/VanillaAnimalsExpandedEndangered/Options/VanillaAnimalsExpandedEndangeredMod.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaAnimalsExpandedEndangered
+{
+    public class VanillaAnimalsExpandedEndangeredMod : Mod
+    {
+        public static VanillaAnimalsExpandedEndangeredSettings settings;
+        public VanillaAnimalsExpandedEndangeredMod(ModContentPack pack) : base(pack)
+        {
+            settings = GetSettings<VanillaAnimalsExpandedEndangeredSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Vanilla Animals Expanded - Endangered";
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            
+        }
+    }
+    
+}
